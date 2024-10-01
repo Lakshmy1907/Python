632c0d0de772861880e0e87ee08e95112ff8f814
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2024-10-01 15:22:49 +0530

    document header added

---
+++ hello python.py
@@ -0,0 +1,14 @@
+'''
+#Author: Lakshmy
+#Date: 01-10-2024
+Python program to get the student details
+Version: 1.0
+'''
+print("Hello Python")
+name=input("Enter your student name")
+roll_number=int(input("Enter roll_number"))
+roll_number=roll_number+1
+student_cgpa=float(input("enter cgpa"))
+print("name of the student",name)
+print("roll_number is,roll_number")
+print("student cgpa:",student_cgpa,"%")
