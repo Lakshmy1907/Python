2e3b72c6ef3743990c70b017e7b16978ca451be7
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2024-10-15 05:50:21 -0400

    Add files via upload

---
+++ simpleinterst.py
@@ -0,0 +1,8 @@
+'''#author: Lakshmy BYju
+#date: 15-10-2024
+find the simple intrest'''
+p=int(input("enter the amount"))
+n=int(input("enter the interst"))
+r=int(input("enter the rate"))
+simpleinterst=(p*n*r)/100
+print(simpleinterst)
