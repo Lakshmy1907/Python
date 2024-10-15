5303f264ca85585bf412f6287874025af39b4f75
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2024-10-15 05:03:24 -0400

    Add files via upload

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+'''#author: Lakshmy Byju
+#date: 15-10-2024
+to check the number is odd or even'''
+
+number=int(input("Enter a number"))
+if number%2==0:
+    print("even")
+else:
+    print("odd")
