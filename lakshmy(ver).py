5f44decee2aa50d6f77c49883e5809e9c7311e47
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2024-11-11 14:21:13 +0530

    Add files via upload

---
+++ lakshmy(ver).py
@@ -0,0 +1,9 @@
+''' author:Lakshmy Byju
+   date : 10-11-2024
+   find the verison of python'''
+   
+import sys
+print("python verison")
+print(sys.version)
+print("version.info")
+print(sys.version_info)
