560e4b1fb8317c3cd368ced44eda04c6eff46c5b
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2024-11-11 14:16:53 +0530

    Add files via upload

--- lakshmy.py
+++ lakshmy.py
@@ -1,15 +1,3 @@
-num1=int(input("Enter num1"))
-num2=int(input("Enter num2"))
-num3=int(input("Enter num3"))
-sum=num1+num2
-print("sum of the number1 and number2:",sum)
-difference=num1-num2
-print("The difference when num2 is subtracted from num1 is:",difference)
-product=num1*num2
-print("The product of num1 and and num2 is:",product)
-quotient=num1/num2
-print("Then quotient when num1 is divided by nmum2 is:",quotient)
-remainder=num1%num2
-print("The remainder when num1 is divided by num2 is:",remainder)
-result=(num1+num2)*num3/2
-print("The result of (num1+num2)*num3/2 is:",result)
+name=input("Enter a name")
+age=int(input("Enter a age"))
+print("hello",name,"you are",age,"years old")
